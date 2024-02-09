08db5cb5691139565639e6f4725846d063fb0e5d
baba-mandef <contact@abiodoun.dev>
2024-02-09 23:01:11 +0100

    ✏️ update user model by adding utils functions(has_role, name)

--- ishiro/user/models.py
+++ ishiro/user/models.py
@@ -1,13 +1,15 @@
 from typing import Any
-from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin, BaseUserManager)
+from django.contrib.auth.models import (
+    AbstractBaseUser, PermissionsMixin, BaseUserManager)
 from django.db import models
 from ishiro.abstract.models import IshiroObject
 from django.utils import timezone
 
+
 class UserManager(BaseUserManager):
 
-    def create(self, email, password=None, **extra_fields) :
-        
+    def create(self, email, password=None, **extra_fields):
+
         if not email:
             raise ValueError('Users must have an email address')
         email = self.normalize_email(email)
@@ -21,7 +23,7 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
-    
+
     def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
@@ -35,8 +37,6 @@ class UserManager(BaseUserManager):
 
         return self.create_user(password, **extra_fields)
 
-        
-    
 
 class User(IshiroObject, AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=50)
@@ -44,7 +44,7 @@ class User(IshiroObject, AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=150, unique=True, db_index=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    
+
     joined = models.DateField(default=timezone.now())
 
     objects = UserManager()
@@ -52,3 +52,17 @@ class User(IshiroObject, AbstractBaseUser, PermissionsMixin):
     EMAIL_FIELDS = ['email']
     USERNAME_FIELD = 'email'
 
+    def has_role(self, perm) -> bool:
+        if self.is_superuser and self.is_active:
+            return True
+        perm = perm
+        role = getattr(self, f"is_{perm}", False)
+        
+        return bool(role) and self.is_active
+    
+    def __str__(self) -> str:
+        return f"{self.public_id.hex} / {self.email}"
+
+    @property
+    def name(self):
+        return f"{self.first_name} {self.last_name}"
