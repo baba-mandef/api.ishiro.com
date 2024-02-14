34fbac7762840d2b86a3af61a3f3195056daa8c2
baba-mandef <contact@abiodoun.dev>
2024-02-14 15:24:53 +0100

    🪛 minor fix

--- ishiro/account/saver/apps.py
+++ ishiro/account/saver/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SaverConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ishiro.acoount.saver'
+    name = 'ishiro.account.saver'
