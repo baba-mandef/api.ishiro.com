2293c9d69d845314b18ea2ee020c4cd0f3df2e8b
baba-mandef <contact@abiodoun.dev>
2024-02-14 15:16:54 +0100

    📝 add saver model

--- ishiro/account/saver/models.py
+++ ishiro/account/saver/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from ishiro.user.models import User
+from ishiro.account.models import AccountMixin
 
-# Create your models here.
+
+class Saver(AccountMixin, User):
+    user = models.OneToOneField(User, parent_link=True, on_delete=models.CASCADE)
+    currency = models.CharField(max_length=10)
+
+  
+
+    class Meta:
+        managed = True
