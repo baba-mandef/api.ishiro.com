41f9519c5484d947292195ebf486613133e92e35
baba-mandef <contact@abiodoun.dev>
2024-02-16 02:17:51 +0100

    ✏️ update wallet model

--- ishiro/wallet/models.py
+++ ishiro/wallet/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from ishiro.abstract.models import IshiroObject
+from ishiro.account.saver.models import Saver
 
-# Create your models here.
+
+class Wallet(models.Model):
+    label = models.CharField(max_length=50)
+    balance = models.IntegerField(default=0)
+    # category = models.ForeignKey()
+    owner = models.ForeignKey(Saver, on_delete=models.CASCADE)
+
