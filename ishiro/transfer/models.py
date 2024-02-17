a7be268e7bc0ed6b91aa024e16d6537a66773bd5
baba-mandef <contact@abiodoun.dev>
2024-02-17 22:48:11 +0100

    📝 add transfer model

--- ishiro/transfer/models.py
+++ ishiro/transfer/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from ishiro.abstract.models import IshiroObject
+from ishiro.wallet.models import Wallet
 
-# Create your models here.
+
+class Transfer(IshiroObject):
+    label = models.CharField(max_length=150)
+    amount = models.IntegerField()
+    description = models.CharField(max_length=500, null=True, blank=True)
+    wallet_source = models.ForeignKey(Wallet, related_name="wallet_source")
+    receiver_wallet = models.ForeignKey(Wallet, related_name="receiver_wallet")
+    
