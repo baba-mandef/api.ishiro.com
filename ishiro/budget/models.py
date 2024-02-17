07b5a9e7c0561bfb0a4b57120fa23df4ca74f952
baba-mandef <contact@abiodoun.dev>
2024-02-17 15:00:36 +0100

    📝 add budget model

--- ishiro/budget/models.py
+++ ishiro/budget/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from ishiro.abstract.models import IshiroObject
+from ishiro.category.models import Category
+from ishiro.account.saver.models import Saver
 
-# Create your models here.
+
+class Budget(IshiroObject):
+    limit = models.IntegerField()
+    category = models.ForeignKey(Category, related_name="budget_category", on_delete=models.CASCADE)
+    spent = models.IntegerField()
+    remaining = models.IntegerField()
+    period = models.DateField()
+    owner = models.ForeignKey(Saver, on_delete=models.CASCADE)
+    
