50d423a16d81cc84a92214416d96cf161803091d
baba-mandef <contact@abiodoun.dev>
2024-02-17 14:56:51 +0100

    📝 add wallet model

--- ishiro/wallet/models.py
+++ ishiro/wallet/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 from ishiro.abstract.models import IshiroObject
 from ishiro.account.saver.models import Saver
+from ishiro.category.models import Category
 
 
-class Wallet(models.Model):
+class Wallet(IshiroObject):
     label = models.CharField(max_length=50)
     balance = models.IntegerField(default=0)
-    # category = models.ForeignKey()
+    category = models.ForeignKey(Category, related_name="wallet_category", on_delete=models.CASCADE)
     owner = models.ForeignKey(Saver, on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return f"{self.label} - {self.balance}"
+
 
