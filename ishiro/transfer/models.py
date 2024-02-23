b0cde2c71c25e2a46f66216da8dbcb21f0bc7716
baba-mandef <contact@abiodoun.dev>
2024-02-23 09:20:28 +0100

    📝 add migration

--- ishiro/transfer/models.py
+++ ishiro/transfer/models.py
@@ -7,6 +7,6 @@ class Transfer(IshiroObject):
     label = models.CharField(max_length=150)
     amount = models.IntegerField()
     description = models.CharField(max_length=500, null=True, blank=True)
-    wallet_source = models.ForeignKey(Wallet, related_name="wallet_source")
-    receiver_wallet = models.ForeignKey(Wallet, related_name="receiver_wallet")
+    wallet_source = models.ForeignKey(Wallet, related_name="wallet_source", on_delete=models.CASCADE)
+    receiver_wallet = models.ForeignKey(Wallet, related_name="receiver_wallet", on_delete=models.CASCADE)
     
