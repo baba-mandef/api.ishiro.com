b654346600c8c897bd1a3793753d8df6cd22ff94
baba-mandef <contact@abiodoun.dev>
2024-02-23 22:52:21 +0100

    📝 add wallet route to router

--- ishiro/routers.py
+++ ishiro/routers.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter
 from ishiro.auth.viewsets import login, register
 from ishiro.category.viewsets import CategoryViewSet
 from ishiro.extra.data.viewsets import PopulateViewSet
+from ishiro.wallet.viewsets import WalletViewSet
 
 
 from django.urls import path, include
@@ -16,6 +17,9 @@ Router.register("register", register.RegisterViewSet, "register")
 #category
 Router.register(r"(?P<category_type>(wallet|income|expense))/category", CategoryViewSet, "category")
 
+
+#wallet
+Router.register("wallet", WalletViewSet, "wallet")
 
 #populate
 Router.register("populate", PopulateViewSet, "populate")
