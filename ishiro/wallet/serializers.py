e7a22c79ff554629a85eaff67b9f864c95ccf327
baba-mandef <contact@abiodoun.dev>
2024-02-24 18:05:28 +0100

    ⚡ optimization

--- ishiro/wallet/serializers.py
+++ ishiro/wallet/serializers.py
@@ -1,23 +1,31 @@
 from rest_framework import serializers
 from ishiro.wallet.models import Wallet
-from ishiro.category.models import Category
-from ishiro.category.serializers import CategorySerializer
+
 
 
 class WalletSerializer(serializers.ModelSerializer):
 
     def validate(self, attrs):
         category = attrs.get('category')
-       
+
         type = category.category_type
         if type != 'wallet':
             raise serializers.ValidationError(
-                    {'category': 'Category type must be a wallet'})
+                {'category': 'Category type must be a wallet'})
         return attrs
+    
+    def create(self, validated_data):
+        wallet = Wallet.objects.create(**validated_data)
+        owner = wallet.owner
+        owner.balance += wallet.balance
+        wallet.save()
+        owner.save()
+        return wallet
 
     class Meta:
         model = Wallet
-        fields = ['public_id', 'label', 'balance', 'category', 'owner']
+        fields = ['id', 'public_id',  'label', 'balance', 'category', 'owner']
         extra_kwargs = {
             'public_id': {'read_only': True, 'required': False, },
+            'id': {'read_only': True, 'required': False, },
         }
--- ishiro/wallet/viewsets.py
+++ ishiro/wallet/viewsets.py
@@ -1,26 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
 from ishiro.wallet.serializers import WalletSerializer
-from ishiro.account.saver.models import Saver
-from rest_framework.response import Response
-from rest_framework import status
-
 
 
 class WalletViewSet(ModelViewSet):
     serializer_class = WalletSerializer
     http_method_names = ['get', 'post',]
-
-    def create(self, request):
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        validated_data = serializer.validated_data
-        balance = validated_data['balance']
-        saver = Saver.objects.filter(user__email=request.user.email).first()
-        saver.balance = saver.balance + balance
-        saver.save()
-        self.serializer_class.Meta.model.objects.create(**validated_data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
 
     def get_queryset(self):
 
