085419ffd1d24467e5a30dc264b493b6ca23c133
baba-mandef <contact@abiodoun.dev>
2024-02-24 19:35:44 +0100

    📝 add activity(income|expense) to router

--- ishiro/routers.py
+++ ishiro/routers.py
@@ -4,6 +4,7 @@ from ishiro.auth.viewsets import login, register
 from ishiro.category.viewsets import CategoryViewSet
 from ishiro.extra.data.viewsets import PopulateViewSet
 from ishiro.wallet.viewsets import WalletViewSet
+from ishiro.activity.viewsets import ActivityViewSet
 
 
 from django.urls import path, include
@@ -17,6 +18,8 @@ Router.register("register", register.RegisterViewSet, "register")
 #category
 Router.register(r"(?P<category_type>(wallet|income|expense))/category", CategoryViewSet, "category")
 
+#activity
+Router.register(r"(?P<activity_type>(income|expense))/activity", ActivityViewSet, "activity")
 
 #wallet
 Router.register("wallet", WalletViewSet, "wallet")
